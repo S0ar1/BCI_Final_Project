7abc9756642596c8814a94a1240303d04a3342f9
S0ar1 <duangx0331@gmail.com>
2022-06-02 19:20:52 +0800

    plot bug fix 修复画图数据错误的bug

--- plot.py
+++ plot.py
@@ -3,9 +3,10 @@ import numpy as np
 
 
 def plt_loss(loss_list):
-    x = np.linspace(1, len(loss_list), len(loss_list))
+    x = np.linspace(1, len(loss_list[0]), len(loss_list[0]))
     plt.figure()
-    plt.plot(x, loss_list)
+    for i in range(len(loss_list)):
+        plt.plot(x, loss_list[i])
     plt.xlabel('epoch')
     plt.ylabel('loss')
     plt.title('the loss of training')
@@ -13,9 +14,10 @@ def plt_loss(loss_list):
 
 
 def plt_train_acc(acc_list):
-    x = np.linspace(1, len(acc_list), len(acc_list))
+    x = np.linspace(1, len(acc_list[0]), len(acc_list[0]))
     plt.figure()
-    plt.plot(x, acc_list)
+    for i in range(len(acc_list)):
+        plt.plot(x, acc_list[i])
     plt.xlabel('epoch')
     plt.ylabel('acc')
     plt.title('the acc of training')
@@ -23,9 +25,10 @@ def plt_train_acc(acc_list):
 
 
 def plt_valid_acc(acc_valid_list):
-    x = np.linspace(1, len(acc_valid_list), len(acc_valid_list))
+    x = np.linspace(1, len(acc_valid_list[0]), len(acc_valid_list[0]))
     plt.figure()
-    plt.plot(x, acc_valid_list)
+    for i in range(len(acc_valid_list)):
+        plt.plot(x, acc_valid_list[i])
     plt.xlabel('epoch')
     plt.ylabel('acc')
     plt.title('the acc of valid')
